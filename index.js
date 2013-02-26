4a5ece09fe19c2393cddfd8f297e38e52095b8f1
jden <jason@denizac.org>
2013-02-26 15:27:09 -0800

    fix typos

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function forEach(promisedArr, iterator) {
   return avec(promisedArr, function (arr) {
     arr.forEach(iterator)
   })
-})
+}
 
 function map(promisedArr, fn) {
   return avec(promisedArr, function (arr) {
     return arr.map(fn)
   })
-})
+}
 
 module.exports = avec
 avec.forEach = forEach
